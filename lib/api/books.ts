57e0ae06730872209dd9100d3b9d55b998338f06
Saba Yar <yars@sheridancollege.ca>
2026-04-14 07:40:52 -0400

    Added createBook, deleteBook, and editBook to books.ts

--- lib/api/books.ts
+++ lib/api/books.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { Book } from "@/lib/api/types"
+import { BookFormData } from "@/lib/validation/booksSchema"
 
 export async function fetchBooks(): Promise<Book[]> {
   const res = await axios.get<Book[]>("http://localhost:8080/api/books")
@@ -8,5 +9,19 @@ export async function fetchBooks(): Promise<Book[]> {
 
 export async function fetchBook(id: string): Promise<Book> {
   const res = await axios.get<Book>(`http://localhost:8080/api/books/${id}`)
+  return res.data
+}
+
+export async function createBook(book: BookFormData) {
+  const res = await axios.post("http://localhost:8080/api/books", book)
+  return res.data
+}
+
+export async function deleteBook(id: string) {
+  await axios.delete(`http://localhost:8080/api/books/${id}`)
+}
+
+export async function editBook(id: string, book: BookFormData) {
+  const res = await axios.put(`http://localhost:8080/api/books/${id}`, book)
   return res.data
 }
